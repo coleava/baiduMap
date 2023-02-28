c0728e2b554c6793b973eeff934c68761c929ba3
cole <wuwei@shgbit.com>
2023-02-28 14:38:08 +0800

    change data source

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 import { Map, Marker, AutoComplete } from 'react-bmapgl'
 import star from './hotel.svg'
 import { useEffect, useState } from 'react'
-import list from './data.json'
 import { Select } from 'antd'
 import './App.css'
 
-const cityList = [
-  { city: '北京市', pois: ['北京王府井希尔顿酒店', '北京希尔顿酒店', '北京大兴希尔顿酒店', '北京通州北投希尔顿酒店', '北京首都机场希尔顿酒店'] },
-  { city: '上海市', pois: ['上海虹桥祥源希尔顿酒店', '上海松江广富林希尔顿酒店', '上海奉贤前昇希尔顿酒店'] },
-  { city: '重庆市', pois: ['重庆希尔顿酒店', '重庆两江新区高科希尔顿酒店'] },
-  { city: '三亚市', pois: ['海南海花岛希尔顿酒店'] },
-  { city: '大连市', pois: ['大连富力希尔顿酒店'] },
-  { city: '广州市', pois: ['佛山希尔顿酒店', '佛山顺德海骏达希尔顿酒店', '广州万富希尔顿酒店', '广州天河希尔顿酒店', '广州翡翠希尔顿酒店'] },
-  { city: '厦门市', pois: ['厦门磐基希尔顿酒店'] },
-  { city: '西安市', pois: ['西安富力希尔顿酒店', '西安高新希尔顿酒店'] },
-  { city: '沈阳市', pois: ['沈阳世茂希尔顿酒店'] },
-  { city: '青岛市', pois: ['青岛金沙滩希尔顿酒店'] },
-  { city: '石家庄市', pois: ['石家庄希尔顿酒店'] },
-  { city: '嘉兴市', pois: ['嘉兴希尔顿酒店'] },
-  { city: '郑州市', pois: ['郑州希尔顿酒店'] },
-  { city: '深圳市', pois: ['深圳国际会展中心希尔顿酒店', '深圳大中华希尔顿酒店'] },
-  { city: '武汉市', pois: ['武汉世茂希尔顿酒店', '武汉光谷希尔顿酒店'] },
-  { city: '天津市', pois: ['天津生态城世茂希尔顿酒店'] },
-  { city: '台州市', pois: ['台州希尔顿酒店'] },
-  { city: '海口市', pois: ['海口鲁能希尔顿酒店', '海口希尔顿酒店', '海南海花岛希尔顿酒店'] },
-  { city: '林芝市', pois: ['工布庄园希尔顿酒店'] },
-  { city: '台北市', pois: ['台北新板希尔顿酒店'] },
-]
+import cityList from './city.json'
+import locations from './location.json'
+
+// const cityList = [
+//   { city: '北京市', pois: ['北京王府井希尔顿酒店', '北京希尔顿酒店', '北京大兴希尔顿酒店', '北京通州北投希尔顿酒店', '北京首都机场希尔顿酒店'] },
+//   { city: '上海市', pois: ['上海虹桥祥源希尔顿酒店', '上海松江广富林希尔顿酒店', '上海奉贤前昇希尔顿酒店'] },
+//   { city: '重庆市', pois: ['重庆希尔顿酒店', '重庆两江新区高科希尔顿酒店'] },
+//   { city: '三亚市', pois: ['海南海花岛希尔顿酒店'] },
+//   { city: '大连市', pois: ['大连富力希尔顿酒店'] },
+//   { city: '广州市', pois: ['佛山希尔顿酒店', '佛山顺德海骏达希尔顿酒店', '广州万富希尔顿酒店', '广州天河希尔顿酒店', '广州翡翠希尔顿酒店'] },
+//   { city: '厦门市', pois: ['厦门磐基希尔顿酒店'] },
+//   { city: '西安市', pois: ['西安富力希尔顿酒店', '西安高新希尔顿酒店'] },
+//   { city: '沈阳市', pois: ['沈阳世茂希尔顿酒店'] },
+//   { city: '青岛市', pois: ['青岛金沙滩希尔顿酒店'] },
+//   { city: '石家庄市', pois: ['石家庄希尔顿酒店'] },
+//   { city: '嘉兴市', pois: ['嘉兴希尔顿酒店'] },
+//   { city: '郑州市', pois: ['郑州希尔顿酒店'] },
+//   { city: '深圳市', pois: ['深圳国际会展中心希尔顿酒店', '深圳大中华希尔顿酒店'] },
+//   { city: '武汉市', pois: ['武汉世茂希尔顿酒店', '武汉光谷希尔顿酒店'] },
+//   { city: '天津市', pois: ['天津生态城世茂希尔顿酒店'] },
+//   { city: '台州市', pois: ['台州希尔顿酒店'] },
+//   { city: '海口市', pois: ['海口鲁能希尔顿酒店', '海口希尔顿酒店', '海南海花岛希尔顿酒店'] },
+//   { city: '林芝市', pois: ['工布庄园希尔顿酒店'] },
+//   { city: '台北市', pois: ['台北新板希尔顿酒店'] },
+//   { city: '杭州市', pois: ['诸暨希尔顿酒店'] }, // 杭州或者绍兴都行
+//   { city: '中山市', pois: ['中山利和希尔顿酒店'] },
+//   { city: '合肥市', pois: ['合肥元一希尔顿酒店'] },
+//   { city: '南京市', pois: ['南京朗昇希尔顿酒店', '南京世茂滨江希尔顿酒店', '南京牛首山希尔顿酒店'] },
+//   //
+// ]
 
 const { Option } = Select
 
 function App() {
   const [one, setOne] = useState()
   const [city, setCity] = useState('上海市')
-  const [map, setMap] = useState(null)
-  const [target, setTarget] = useState(null)
 
-  const onSelect = (uid) => {
-    let one = list.find((p) => p.uid === uid)
-    setOne(one)
-  }
+  const [map, setMap] = useState(null)
+
+  //   const onSelect = (uid) => {
+  //     let one = list.find((p) => p.uid === uid)
+  //     setOne(one)
+  //   }
 
   useEffect(() => {
+    // cityChange()
     initData('上海市')
   }, [])
 
@@ -54,73 +62,129 @@ function App() {
     // var point = new window.BMapGL.Point(116.4133836971231,39.910924547299565) // 北京
 
     // var point = new window.BMapGL.Point( 121.48053886017651,31.235929042252014) // 上海
-    // map.centerAndZoom(val, 10)
-    var myGeo = new window.BMapGL.Geocoder()
 
-    // 将地址解析结果显示在地图上,并调整地图视野
-    let { pois } = cityList.find((x) => x.city === val)
-    // locations
-    pois.forEach((poi) => {
-      myGeo.getPoint(
-        poi,
-        (point) => {
-          if (point) {
-            let location = new window.BMapGL.Point(point.lng, point.lat)
-            let marker = new window.BMapGL.Marker(location)
+    let location = locations.find((local) => local.location === val)
+    // console.log('location', map)
+    location.list.forEach((local) => {
+      let point = new window.BMapGL.Point(local.point.lng, local.point.lat)
+      let marker = new window.BMapGL.Marker(point)
+      console.log('local', local)
+      marker.onclick = (e) => {
+        var localSearch = new window.BMapGL.LocalSearch(val, {
+          // renderOptions: { map },
+          onSearchComplete: (results) => {
+            //   console.log('result', results)
+            let find = results._pois.find((result) => result.title.endsWith('希尔顿酒店') || result.title.endsWith(')'))
+            console.log('find', find)
 
-            marker.onclick = (e) => {
-              //   let infoWindow = new window.BMapGL.InfoWindow(poi, {
-              //     width: 200,
-              //     height: 50,
-              //     title: poi,
-              //   })
-              //   map.openInfoWindow(infoWindow, point)
-              //   map.centerAndZoom(point, 14)
-              var local = new window.BMapGL.LocalSearch(val, {
-                // renderOptions: { map },
-                onSearchComplete: (results) => {
-                  console.log(results)
-                  let find = results._pois.find((result) => (result.title.endsWith('希尔顿酒店') || result.title.endsWith(')')) && result.isAccurate)
-                  console.log('find', find)
-
-                  var opts = {
-                    width: 280, // 信息窗口宽度
-                    height: 100, // 信息窗口高度
-                    title: find.title, // 信息窗口标题
-                  }
-                  let infoWindow = new window.BMapGL.InfoWindow(
-                    (
-                     ` <div style="padding: 10px 0">
+            // find && sourcelist.push(find)
+            var opts = {
+              width: 300, // 信息窗口宽度
+              height: 100, // 信息窗口高度
+              title: find.title, // 信息窗口标题
+            }
+            let infoWindow = new window.BMapGL.InfoWindow(
+              ` <div style="padding: 10px 0">
                         <div style="margin-bottom: 4px">地址:  ${find.address}</div>
                         <div>电话:  ${find.phoneNumber}</div>
-                      </div>`
-                    ),
-                    opts
-                  )
-                  infoWindow.onclickclose = () => {
-                    map.centerAndZoom(val, 10)
-                  }
-                  map.openInfoWindow(infoWindow, location)
-                  map.centerAndZoom(location, 14)
-                },
-              })
-              local.search(poi)
+                      </div>`,
+              opts
+            )
+            infoWindow.onclickclose = () => {
+              map.centerAndZoom(val, 10)
             }
-            map.addOverlay(marker)
-          } else {
-            alert('您选择地址没有解析到结果!')
-          }
-        },
-        val
-      )
+            map.openInfoWindow(infoWindow, point)
+            map.centerAndZoom(point, 14)
+          },
+        })
+        localSearch.search(local.title)
+      }
+      map.centerAndZoom(val, 11)
+      map.addOverlay(marker)
     })
-    map.centerAndZoom(val, 11)
-    setMap(map)
+
+    // 将地址解析结果显示在地图上,并调整地图视野
+    // let { pois } = cityList.find((x) => x.city === val)
+    // console.log('pois',pois);
+    // let myGeo = new window.BMapGL.Geocoder()
+    // pois.forEach((poi) => {
+    //   myGeo.getPoint(
+    //     poi,
+    //     (point) => {
+    //   if (point) {
+    // let location = new window.BMapGL.Point(point.lng, point.lat)
+    // let marker = new window.BMapGL.Marker(location)
+
+    // marker.onclick = (e) => {
+    //   var local = new window.BMapGL.LocalSearch(val, {
+    //     // renderOptions: { map },
+    //     onSearchComplete: (results) => {
+    //       console.log('result', results)
+    //       let find = results._pois.find((result) => result.title.endsWith('希尔顿酒店') || result.title.endsWith(')'))
+    //       // console.log('find', find)
+
+    //       // find && sourcelist.push(find)
+    //       var opts = {
+    //         width: 300, // 信息窗口宽度
+    //         height: 100, // 信息窗口高度
+    //         title: find.title, // 信息窗口标题
+    //       }
+    //       let infoWindow = new window.BMapGL.InfoWindow(
+    //         ` <div style="padding: 10px 0">
+    //                 <div style="margin-bottom: 4px">地址:  ${find.address}</div>
+    //                 <div>电话:  ${find.phoneNumber}</div>
+    //               </div>`,
+    //         opts
+    //       )
+    //       infoWindow.onclickclose = () => {
+    //         map.centerAndZoom(val, 10)
+    //       }
+    //       map.openInfoWindow(infoWindow, location)
+    //       map.centerAndZoom(location, 14)
+    //     },
+    //   })
+    //   local.search(poi)
+    // }
+    // map.addOverlay(marker)
+    //   } else {
+    //     alert('您选择地址没有解析到结果!')
+    //   }
+    // }
+    //     val
+    //   )
+    // })
+    // map.centerAndZoom(val, 11)
+    // setMap(map)
   }
 
   const cityChange = (value) => {
     setCity(value || '上海市')
+
     initData(value || '上海市')
+    // initData('上海市')
+    // initData('北京市')
+    // initData('重庆市')
+    // initData('三亚市')
+    // initData('大连市')
+    // initData('广州市')
+    // initData('厦门市')
+    // initData('西安市')
+    // initData('沈阳市')
+    // initData('青岛市')
+    // initData('石家庄市')
+    // initData('嘉兴市')
+    // initData('郑州市')
+    // initData('深圳市')
+    // initData('武汉市')
+    // initData('天津市')
+    // initData('台州市')
+    // initData('海口市')
+    // initData('林芝市')
+    // initData('台北市')
+    // initData('杭州市')
+    // initData('中山市')
+    // initData('合肥市')
+    // initData('南京市')
   }
 
   return (
