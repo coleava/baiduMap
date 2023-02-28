566c62c6127a379e86f67a312a06e883ce72dd85
cole <wuwei@shgbit.com>
2023-02-28 22:29:59 +0800

    detail / list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { Map, Marker, AutoComplete } from 'react-bmapgl'
-import star from './hotel.svg'
-import { useEffect, useState } from 'react'
-import { Select } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Select, Divider, Rate } from 'antd'
 import './App.css'
+import GeryPng from './imgs/location-grey.png'
+import TelPng from './imgs/tel.png'
+import BackSvg from './imgs/back.svg'
 
-// import cityList from './city.json'
 import locations from './location.json'
 
 const { Option } = Select
 
 function App() {
-  const [one, setOne] = useState()
   const [city, setCity] = useState('上海市')
 
   const [map, setMap] = useState(null)
-
-  //   const onSelect = (uid) => {
-  //     let one = list.find((p) => p.uid === uid)
-  //     setOne(one)
-  //   }
-
+  const [targetList, setTargetList] = useState([])
+  const [showDetail, setShowDetail] = useState(false)
+  const [selectLocation, setSelectLocation] = useState(null)
+  const [point, setPoint] = useState(null)
+  const [selectMarker, setMarker] = useState(null)
   useEffect(() => {
-    // cityChange()
     initData('上海市')
   }, [])
 
@@ -35,31 +32,41 @@ function App() {
 
     // var point = new window.BMapGL.Point( 121.48053886017651,31.235929042252014) // 上海
     let location = locations.find((local) => local.location === val)
+    setTargetList(location.list)
     location.list.forEach((local) => {
       let point = new window.BMapGL.Point(local.point.lng, local.point.lat)
-      let marker = new window.BMapGL.Marker(point)
+      let myIcon = new window.BMapGL.Icon(require('./imgs/location-red.png'), new window.BMapGL.Size(32, 32))
+      let marker = new window.BMapGL.Marker(point, { icon: myIcon })
+      //   marker.setImageUrl(require('./imgs/location-red.png'))
       marker.onclick = (e) => {
         var localSearch = new window.BMapGL.LocalSearch(val, {
           // renderOptions: { map },
           onSearchComplete: (results) => {
             let find = results._pois.find((result) => result.title.endsWith('希尔顿酒店') || result.title.endsWith(')'))
-            var opts = {
-              width: 300, // 信息窗口宽度
-              height: 100, // 信息窗口高度
-              title: find.title, // 信息窗口标题
-            }
-            let infoWindow = new window.BMapGL.InfoWindow(
-              ` <div style="padding: 10px 0">
-                        <div style="margin-bottom: 4px">地址:  ${find.address}</div>
-                        <div>电话:  ${find.phoneNumber}</div>
-                      </div>`,
-              opts
-            )
-            infoWindow.onclickclose = () => {
-              map.centerAndZoom(val, 10)
-            }
-            map.openInfoWindow(infoWindow, point)
+            find['image'] = local.image
+            find['rating'] = local.rating
+            marker.setIcon(new window.BMapGL.Icon(require('./imgs/location-blue.png'), new window.BMapGL.Size(32, 32)))
+            setMarker(marker)
+            // marker.setImageUrl(require('./imgs/location-blue.png'))
+            // var opts = {
+            //   width: 300, // 信息窗口宽度
+            //   height: 100, // 信息窗口高度
+            //   title: find.title, // 信息窗口标题
+            // }
+            // let infoWindow = new window.BMapGL.InfoWindow(
+            //   ` <div style="padding: 10px 0">
+            //             <div style="margin-bottom: 4px">地址:  ${find.address}</div>
+            //             <div>电话:  ${find.phoneNumber}</div>
+            //           </div>`,
+            //   opts
+            // )
+            // infoWindow.onclickclose = () => {
+            //   map.centerAndZoom(val, 10)
+            // }
+            // map.openInfoWindow(infoWindow, point)
             map.centerAndZoom(point, 14)
+            selectTarget(find)
+            setPoint(point)
           },
         })
         localSearch.search(local.title)
@@ -67,7 +74,7 @@ function App() {
       map.centerAndZoom(point, 11)
       map.addOverlay(marker)
     })
-
+    setMap(map)
     // 将地址解析结果显示在地图上,并调整地图视野
     // let { pois } = cityList.find((x) => x.city === val)
     // console.log('pois',pois);
@@ -124,71 +131,95 @@ function App() {
 
   const cityChange = (value) => {
     setCity(value || '上海市')
-
     initData(value || '上海市')
-    // initData('上海市')
-    // initData('北京市')
-    // initData('重庆市')
-    // initData('三亚市')
-    // initData('大连市')
-    // initData('广州市')
-    // initData('厦门市')
-    // initData('西安市')
-    // initData('沈阳市')
-    // initData('青岛市')
-    // initData('石家庄市')
-    // initData('嘉兴市')
-    // initData('郑州市')
-    // initData('深圳市')
-    // initData('武汉市')
-    // initData('天津市')
-    // initData('台州市')
-    // initData('海口市')
-    // initData('林芝市')
-    // initData('台北市')
-    // initData('杭州市')
-    // initData('中山市')
-    // initData('合肥市')
-    // initData('南京市')
+    setShowDetail(false)
+    setSelectLocation(null)
+  }
+
+  const selectTarget = (target) => {
+    console.log(target)
+    setSelectLocation(target)
+    setShowDetail(true)
+    let point = new window.BMapGL.Point(target.point.lng, target.point.lat)
+    map.centerAndZoom(point, 14)
+    let myIcon = new window.BMapGL.Icon(require('./imgs/location-blue.png'), new window.BMapGL.Size(32, 32))
+    let marker = new window.BMapGL.Marker(point, { icon: myIcon })
+    map.addOverlay(marker)
+    setMarker(marker)
+    setPoint(point)
+  }
+
+  const back = () => {
+    setShowDetail(false)
+    point && map.centerAndZoom(point, 11)
+    selectMarker && selectMarker.setIcon(new window.BMapGL.Icon(require('./imgs/location-red.png'), new window.BMapGL.Size(32, 32)))
   }
 
   return (
     <div style={{ position: 'relative' }}>
-      {/* <div style={{ zIndex: 100, position: 'absolute', width: 500, height: 200, bottom: 0, background: '#ffffff', overflow: 'auto' }}>
-        {list.map((item) => {
-          return (
-            <div
-              key={item.uid}
-              style={{ display: 'flex', flexDirection: 'column', padding: 10, height: 30, borderBottom: '1px solid #efefef', background: one && item.uid === one.uid ? '#f9a216' : '', cursor: 'pointer' }}
-              onClick={() => onSelect(item.uid)}>
-              <span style={{ fontSize: 14 }}>{item.name}</span>
-              <span style={{ fontSize: 8, color: 'gray' }}>{item.address}</span>
+      <div style={{ position: 'absolute', top: 20, left: 20, zIndex: 999, width: '45%', height: '75vh' }}>
+        <Select style={{ width: '100%' }} value={city} onChange={cityChange} allowClear>
+          {locations.map((item, index) => {
+            return (
+              <Option value={item.location} key={index}>
+                {item.location}
+              </Option>
+            )
+          })}
+        </Select>
+        <div className="into" style={{ height: targetList.length > 3 ? '100%' : '75%' }}>
+          {!showDetail &&
+            targetList.map((target, index) => {
+              return (
+                <div key={index} style={{ cursor: 'pointer' }} onClick={() => selectTarget(target)}>
+                  <div className="title">{target.title}</div>
+                  <div className="content">
+                    <div className="type">
+                      <div style={{ paddingBottom: 8 }}>
+                        {target.rating.toFixed(1)}&nbsp;/&nbsp;5分
+                        <Divider type="vertical" />
+                        豪华型
+                      </div>
+                      <div>{target.address}</div>
+                    </div>
+                    <div style={{ flex: 1, marginRight: 8 }}>
+                      <img src={target.image} width="95%" height="80" />
+                    </div>
+                  </div>
+                  <Divider style={{ margin: '12px 0 10px 0' }} />
+                </div>
+              )
+            })}
+
+          {showDetail && (
+            <div className="detail">
+              <div>
+                <div id="placereturnfixed" onClick={() => back()}>
+                  <img src={BackSvg} width="16" style={{ marginRight: 4 }} />
+                  返回
+                </div>
+                <img src={selectLocation.image} width="100%" height="200" />
+              </div>
+              <div style={{ fontSize: 22, fontWeight: 'bold', padding: '8px 0' }}>{selectLocation.title}</div>
+              <div>
+                <Rate allowHalf value={Number(selectLocation.rating)} style={{ color: 'red' }} />
+                <span style={{ marginLeft: 16 }}>{selectLocation.rating}分</span>
+                <span style={{ marginLeft: 16 }}> 高档型</span>
+              </div>
+
+              <div style={{ padding: '16px 0', display: 'flex', alignItems: 'center' }}>
+                <img src={GeryPng} width="24" />
+                <span style={{ marginLeft: 16, fontSize: 16 }}>{selectLocation.address}</span>
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={TelPng} width="24" />
+                <span style={{ marginLeft: 16, fontSize: 16 }}>{selectLocation.phoneNumber}</span>
+              </div>
             </div>
-          )
-        })}
-      </div> */}
-      <Select style={{ width: 200, position: 'absolute', top: 20, left: 20, zIndex: 999 }} value={city} onChange={cityChange} allowClear>
-        {locations.map((item, index) => {
-          return (
-            <Option value={item.location} key={index}>
-              {item.location}
-            </Option>
-          )
-        })}
-      </Select>
+          )}
+        </div>
+      </div>
       <div id="container"></div>
-      {/* <Map
-        center={one ? new window.BMapGL.Point(one.location.lng, one.location.lat) : new window.BMapGL.Point(106.560162, 29.561205)}
-        zoom={one ? 14 : 6}
-        autoViewport={false}
-        enableScrollWheelZoom={true}
-        enableDragging={true}
-        enableDoubleClickZoom={true}
-        style={{ height: '100vh' }}>
-        {list.map((p) => {
-          return <Marker onClick={() => onSelect(p.uid)} key={p.uid} icon={new window.BMapGL.Icon(star, new window.BMapGL.Size(30, 30))} position={new window.BMapGL.Point(p.location.lng, p.location.lat)} />
-        })}
-      </Map> */}
       {/* <Map
         center={one ? new window.BMapGL.Point(one.location.lng, one.location.lat) : new window.BMapGL.Point(116.4133836971231,39.910924547299565)}
         zoom={one ? 14 : 6}
